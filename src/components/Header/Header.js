f2feb7ceb5825aef768caac01c3fb5121d89842c
Wilson Pinto <wilsonpinto88@gmail.com>
2023-03-08 11:38:32 +0000

    made a small correction in the header section. created 3 lists instead of just one

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -19,9 +19,13 @@ const Header = () =>  (
         <Link href="#projects">
           <NavLink>Projects</NavLink>
         </Link>
+      </li>
+      <li>
         <Link href="#tech">
           <NavLink>Technologies</NavLink>
         </Link>
+      </li>
+      <li>
         <Link href="#about">
           <NavLink>About</NavLink>
         </Link>
